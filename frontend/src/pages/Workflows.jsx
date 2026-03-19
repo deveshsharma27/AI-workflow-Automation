7330d71c494ad7b1eb3154d44d3737958b57c583
Devesh Sharma <devesh200300@gmail.com>
2026-03-19 19:54:21 +0530

    fix: resolve WorkflowCard import issue

--- frontend/src/pages/Workflows.jsx
+++ frontend/src/pages/Workflows.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
 import AppShell from '../components/Appshell'
-import WorkflowCard from '../components/workflowCard.jsx';
+import WorkflowCard from "../components/WorkflowCard";
 
 import { workflowAPI } from '../utils/api'
 import '../styles/Workflows.css'
